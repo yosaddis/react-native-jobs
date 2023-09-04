a3abaecc5c1a739510cec501fe5dc23e92b80bb2
yosaddis <email.to.yoseph@gmail.com>
2023-09-04 16:04:47 +0300

    Added external fonts

--- app/index.js
+++ app/index.js
@@ -1,10 +1,46 @@
-import { View, Text} from 'react-native';
+import { useState } from 'react';
+import {View, ScrollView, SafeAreaView} from 'react-native';
+import{Stack, useRouter} from 'expo-router';
+
+import {COLORS, SIZES, icons, images} from '../constants';
+import {Nearbyjobs, PopularJobs, Popularjobs, ScreenHeaderBtn, Welcome
+} from '../components';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
+
 
 const Home = () => {
+    const router = useRouter();
+
     return(
-        <View>
-            <Text>Home</Text>
-        </View>
+        <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite}}>            
+            <Stack.Screen
+                options={{headerStyle: {backgroundColor: COLORS.lightWhite},
+                    headerShadowVisible: false,
+                    headerLeft: () => (
+                        <ScreenHeaderBtn iconUrl = {icons.menu} dimension ="60%" />
+                    ),
+                    headerRight: () => (
+                        <ScreenHeaderBtn iconUrl = {images.profile} dimension ="100%" />
+                    ),
+                    headerTitle: ""
+                    }}>
+            </Stack.Screen>
+            <ScrollView showsVerticalScrollIndicator={false}>
+                <View
+                    style={{
+                        flex:1,
+                        padding: SIZES.medium
+                    }}
+                >
+                    <Welcome
+
+                    />
+                    <Popularjobs/>
+                    <Nearbyjobs/>
+
+                </View>
+            </ScrollView>
+        </SafeAreaView>
     )
 }
 
