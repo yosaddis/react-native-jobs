9ddbd02bd947d4238b92409ffae905d1907145bc
yosaddis <email.to.yoseph@gmail.com>
2023-09-06 17:10:42 +0300

    Add hook usefetch.js for populat jobs

--- hook/useFetch.js
+++ hook/useFetch.js
@@ -8,60 +8,37 @@ const useFetch = (endpoint,query) => {
     const [data, setData] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
     const [error, setError] = useState(null);
-
     
     const options = {
         method: 'GET',
-        url: 'https://jsearch.p.rapidapi.com/search',
-        params: {
-          query: 'Python developer in Texas, USA',
-          page: '1',
-          num_pages: '1'
-        },
+        url: `https://jsearch.p.rapidapi.com/${endpoint}`,
         headers: {
           'X-RapidAPI-Key': '94c376bb06msh0ebe9da93af7056p199cc1jsn357ff5e8a4fc',
           'X-RapidAPI-Host': 'jsearch.p.rapidapi.com'
-        }
+        },
+        params: { ...query },
       };
 
-     
-
-    // const options = {
-    //     method: 'GET',
-    //     url: 'https://jsearch.p.rapidapi.com/search',
-    //     params: { ...query },
-    //     headers: {
-    //       'X-RapidAPI-Key': '94c376bb06msh0ebe9da93af7056p199cc1jsn357ff5e8a4fc',
-    //       'X-RapidAPI-Host': 'jsearch.p.rapidapi.com'
-    //     }
-    //   };
-
     const fetchData = async () => {
-       
+
+        setIsLoading(true);
         try {
             const response = await axios.request(options);
-            console.log(response.data);
+            setData(response.data.data);
+            //console.log("Response: "+response.data.data);
+            setIsLoading(false);   
         } catch (error) {
-            console.error(error);
+            setError(error);
+            alert('There is an error '+ error )
+            //console.log(options);
         }
-       
-        // setIsLoading(true);
-        // try {
-        //     const response = await axios.request(options);
-        //     setData(response.data.data);
-        //     setIsLoading(false);   
-        // } catch (error) {
-        //     setError(error);
-        //     alert('There is an error'+options.json)
-        //     console.log(options);
-        // }
-        // finally{
-        //     setIsLoading(false);
-        // }
+        finally{
+            setIsLoading(false);
+        }
     }
-    // useEffect(() => {
-    //     fetchData();
-    // }, []);  
+    useEffect(() => {
+        fetchData();
+    }, []);  
 
     const refetch = () => {
         setIsLoading(true);
