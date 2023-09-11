c7c90f8b8da24ed7521d02d7adfdb800f2771431
yosaddis <email.to.yoseph@gmail.com>
2023-09-11 16:50:35 +0300

    Modified compnenets to have job details page

--- components/common/header/ScreenHeaderBtn.jsx
+++ components/common/header/ScreenHeaderBtn.jsx
@@ -1,16 +1,15 @@
-import React from 'react'
 import { TouchableOpacity, Image } from 'react-native'
 
 import styles from './screenheader.style'
 
 
-const ScreenHeaderBtn = ( { iconUrl, dimensions, handlePress } ) => {
+const ScreenHeaderBtn = ( { iconUrl, dimension, handlePress } ) => {
   return (
-    <TouchableOpacity style={styles.btnContainer } onPress={()=>{ alert("touched me!") }}>
+    <TouchableOpacity style={styles.btnContainer } onPress={handlePress}>
       <Image
         source={iconUrl }
         resizeMode="cover"
-        style={styles.btnImg(dimensions)}
+        style={styles.btnImg(dimension)}
       />
     </TouchableOpacity>
   )
--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -19,8 +19,13 @@ const Popularjobs = () => {
     num_pages: 1, 
   })
 
- // console.log(data);
 
+  const [selectedJob, setSelectedJob] = useState();
+
+ const handleCardPress = (item) => {
+  router.push(`/job-details/${item.job_id}`);
+  setSelectedJob(item.job_id);
+};
   return (
     <View style= {styles.container}>
       <View style={styles.header}>
@@ -40,7 +45,8 @@ const Popularjobs = () => {
           renderItem={({ item }) => (
             <PopularJobCard
               item={item}
-              
+              selectedJob={selectedJob}
+              handleCardPress={handleCardPress}
               />
           )}
           keyExtractor={item => item.job_id}
